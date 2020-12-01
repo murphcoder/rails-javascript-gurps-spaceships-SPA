3323230e133f4a29a8192891e08bb813ceea4b1c
murphcoder <murphcoder@gmail.com>
2020-12-01 06:26:48 -0600

    hull

--- gurps-spaceships-frontend/src/index.js
+++ gurps-spaceships-frontend/src/index.js
@@ -9,7 +9,22 @@ class Spaceship {
         this.user_id = ship.user_id;
         this.created_at = ship.created_at;
         this.updated_at = ship.updated_at;
-        
+        this.hulls = ship.hulls.map(function(hull) {return new Hull(hull, this)}.bind(this));
+        this.features = ship.features.map(function(feature) {return feature.id});
+        this.switches = ship.switches.map(function(rule) {return rule.id});
+        this.constructor.all.push(this);
     };
 }
+
+Spaceship.all = [];
+
+class Hull {
+    constructor(hull, spaceship) {
+        this.section = hull.section;
+        this.spaceship = spaceship;
+        this.placements = hull.placements.map(function(placement) {return new placement(placement, this)}.bind(this));
+    }
+}
+
+Hull.all = [];
 
